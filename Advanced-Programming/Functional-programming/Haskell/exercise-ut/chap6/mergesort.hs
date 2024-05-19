d2dc5818e4643f5166707f50c5997ee095d5474a
Nam <khanhnamgalaxy@gmail.com>
2024-05-19 11:08:19 +0700

    random push

--- Advanced-Programming/Functional-programming/Haskell/exercise-ut/chap6/mergesort.hs
+++ Advanced-Programming/Functional-programming/Haskell/exercise-ut/chap6/mergesort.hs
@@ -1,10 +1,17 @@
 merge :: Ord a => [a] -> [a] -> [a]
+merge [] [] = []
 merge xs [] = xs
 merge [] ys = ys
-merge (x:xs) (y:ys) = m
-    where
-        m = if x < y then [x] ++ merge xs (y:ys) else [y] ++ merge (x:xs) ys
+merge (x:xs) (y:ys) 
+    | x <= y = [x] ++ merge (xs) (y:ys)
+    | otherwise = [y] ++ merge (x:xs) (ys)
+
+halve :: Ord a => [a] -> ([a],[a])
+halve xs = (take lhs xs,drop lhs xs)
+    where lhs = length xs `div` 2
 
 mergesort :: Ord a => [a] -> [a]
 mergesort [] = []
-mergesort xs = ms ++ ns 
+mergesort [x] = [x]
+mergesort xs = merge (mergesort left) (mergesort right)
+    where (left,right) = halve xs
